f52746f06548c22cb0862725abc972d279caf6cd
olasrum <mursalo@gmail.com>
2023-04-05 16:00:04 +1000

    updated website link

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="header">
+            <h1>Full stack project</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/SideBar.js
@@ -0,0 +1,9 @@
+function SideBar() {
+    return (
+        <div className="sidebar">
+            <img className="sidepic" src="./sidebar-bird.jpg" alt="colorful bird image"></img>
+        </div>
+    )
+}
+
+export default SideBar;
